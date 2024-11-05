2246d1bcea46ab3d87aa2acc9b9a0a748153712f
24HP7482@nest.edu.mn <24HP7482nest.edu.mn@G304-CC0029.local>
2024-11-05 18:30:44 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+app.use(express.json());
+
+const users = [
+  {
+    id: 1,
+    name: "Bat",
+  },
+  {
+    id: 2,
+    name: "Bold",
+  },
+  {
+    id: 3,
+    name: "ANu",
+  },
+];
+app.get("/users", (req, res) => {
+  const token = req.headers.authorization;
+  console.log(token);
+  jwt.verify(token, "secret", (err) => {
+    if (err) {
+      res.send("invalid toke");
+    }
+  });
+  if (token) {
+    res.send(users);
+  } else {
+    res.send("ajillahgui bn");
+  }
+});
+
+app.post("/login", (req, res) => {
+  const body = req.body;
+  const token = jwt.sign(body, "secret");
+  console.log(body);
+  res.send(token);
+});
+
+app.listen(8080, console.log("server is running"));
